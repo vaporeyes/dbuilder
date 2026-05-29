aad31b28f46602dc073960bf74ec2153a3f89d79
jsh dncn <pythonicqualms@gmail.com>
2026-05-29 06:07:44 -0500

    Add map drawing override toggles

--- docs/TODO.md
+++ docs/TODO.md
@@ -359,6 +359,7 @@ This document tracks the remaining work to bring DBuilder to feature parity with
 - [ ] Port map options dialog.
   - [x] Add basic map marker and UDMF namespace dialog.
   - [x] Add drawing default fields to the map options dialog.
+  - [x] Add drawing override toggles to the map options dialog.
 - [ ] Port open map options dialog.
   - [x] Prompt for initial map selection when opening multi-map WAD or PK3 archives.
 - [x] Port preferences dialog.
--- src/DBuilder.Editor/MapOptionsDialog.cs
+++ src/DBuilder.Editor/MapOptionsDialog.cs
@@ -18,6 +18,14 @@ public sealed class MapOptionsDialog : PropertyDialog
     private readonly TextBox _floorHeight;
     private readonly TextBox _ceilingHeight;
     private readonly TextBox _brightness;
+    private readonly CheckBox _overrideFloorTexture;
+    private readonly CheckBox _overrideCeilingTexture;
+    private readonly CheckBox _overrideTopTexture;
+    private readonly CheckBox _overrideMiddleTexture;
+    private readonly CheckBox _overrideBottomTexture;
+    private readonly CheckBox _overrideFloorHeight;
+    private readonly CheckBox _overrideCeilingHeight;
+    private readonly CheckBox _overrideBrightness;
     private readonly CheckBox _useLongTextureNames;
     private readonly bool _longTextureNamesSupported;
 
@@ -41,6 +49,14 @@ public sealed class MapOptionsDialog : PropertyDialog
         _floorHeight = AddField("Floor height", options.CustomFloorHeight.ToString(System.Globalization.CultureInfo.InvariantCulture));
         _ceilingHeight = AddField("Ceiling height", options.CustomCeilingHeight.ToString(System.Globalization.CultureInfo.InvariantCulture));
         _brightness = AddField("Brightness", options.CustomBrightness.ToString(System.Globalization.CultureInfo.InvariantCulture));
+        _overrideFloorTexture = AddCheckBox("Override floor texture", options.OverrideFloorTexture);
+        _overrideCeilingTexture = AddCheckBox("Override ceiling texture", options.OverrideCeilingTexture);
+        _overrideTopTexture = AddCheckBox("Override upper texture", options.OverrideTopTexture);
+        _overrideMiddleTexture = AddCheckBox("Override middle texture", options.OverrideMiddleTexture);
+        _overrideBottomTexture = AddCheckBox("Override lower texture", options.OverrideBottomTexture);
+        _overrideFloorHeight = AddCheckBox("Override floor height", options.OverrideFloorHeight);
+        _overrideCeilingHeight = AddCheckBox("Override ceiling height", options.OverrideCeilingHeight);
+        _overrideBrightness = AddCheckBox("Override brightness", options.OverrideBrightness);
         _useLongTextureNames = AddCheckBox("Use long texture names", longTextureNamesSupported && options.UseLongTextureNames);
         _useLongTextureNames.IsEnabled = longTextureNamesSupported;
     }
@@ -55,6 +71,14 @@ public sealed class MapOptionsDialog : PropertyDialog
         options.CustomFloorHeight = ParseInt(_floorHeight, options.CustomFloorHeight);
         options.CustomCeilingHeight = ParseInt(_ceilingHeight, options.CustomCeilingHeight);
         options.CustomBrightness = System.Math.Clamp(ParseInt(_brightness, options.CustomBrightness), 0, 255);
+        options.OverrideFloorTexture = _overrideFloorTexture.IsChecked == true;
+        options.OverrideCeilingTexture = _overrideCeilingTexture.IsChecked == true;
+        options.OverrideTopTexture = _overrideTopTexture.IsChecked == true;
+        options.OverrideMiddleTexture = _overrideMiddleTexture.IsChecked == true;
+        options.OverrideBottomTexture = _overrideBottomTexture.IsChecked == true;
+        options.OverrideFloorHeight = _overrideFloorHeight.IsChecked == true;
+        options.OverrideCeilingHeight = _overrideCeilingHeight.IsChecked == true;
+        options.OverrideBrightness = _overrideBrightness.IsChecked == true;
         options.UseLongTextureNames = _longTextureNamesSupported && _useLongTextureNames.IsChecked == true;
     }
 
