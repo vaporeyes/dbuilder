1fdf00447f37e61db67c2b7d8a3a54fdae0ea09e
jsh dncn <pythonicqualms@gmail.com>
2026-06-05 09:16:49 -0500

    Use UDB defaults for sector height shortcuts

--- tests/DBuilder.Tests/EditorCommandCatalogTests.cs
+++ tests/DBuilder.Tests/EditorCommandCatalogTests.cs
@@ -2510,30 +2510,43 @@ public class EditorCommandCatalogTests
     }
 
     [Theory]
-    [InlineData("map2d.lower-floor-8", "Lower Floor by 8 mp")]
-    [InlineData("map2d.lowerfloor8", "Lower Floor by 8 mp")]
-    [InlineData("map2d.raise-floor-8", "Raise Floor by 8 mp")]
-    [InlineData("map2d.raisefloor8", "Raise Floor by 8 mp")]
-    [InlineData("map2d.lower-ceiling-8", "Lower Ceiling by 8 mp")]
-    [InlineData("map2d.lowerceiling8", "Lower Ceiling by 8 mp")]
-    [InlineData("map2d.raise-ceiling-8", "Raise Ceiling by 8 mp")]
-    [InlineData("map2d.raiseceiling8", "Raise Ceiling by 8 mp")]
-    [InlineData("map2d.raise-brightness-8", "Increase Brightness by 8")]
-    [InlineData("map2d.raisebrightness8", "Increase Brightness by 8")]
-    [InlineData("map2d.lower-brightness-8", "Decrease Brightness by 8")]
-    [InlineData("map2d.lowerbrightness8", "Decrease Brightness by 8")]
-    public void SectorHeightCommandsMatchUdbActionSurface(string id, string title)
+    [InlineData("map2d.lower-floor-8", "Lower Floor by 8 mp", "Menu")]
+    [InlineData("map2d.lowerfloor8", "Lower Floor by 8 mp", "Ctrl/Cmd+Alt+ScrollDown")]
+    [InlineData("map2d.raise-floor-8", "Raise Floor by 8 mp", "Menu")]
+    [InlineData("map2d.raisefloor8", "Raise Floor by 8 mp", "Ctrl/Cmd+Alt+ScrollUp")]
+    [InlineData("map2d.lower-ceiling-8", "Lower Ceiling by 8 mp", "Menu")]
+    [InlineData("map2d.lowerceiling8", "Lower Ceiling by 8 mp", "Alt+Shift+ScrollDown")]
+    [InlineData("map2d.raise-ceiling-8", "Raise Ceiling by 8 mp", "Menu")]
+    [InlineData("map2d.raiseceiling8", "Raise Ceiling by 8 mp", "Alt+Shift+ScrollUp")]
+    [InlineData("map2d.raise-brightness-8", "Increase Brightness by 8", "Menu")]
+    [InlineData("map2d.raisebrightness8", "Increase Brightness by 8", "Menu")]
+    [InlineData("map2d.lower-brightness-8", "Decrease Brightness by 8", "Menu")]
+    [InlineData("map2d.lowerbrightness8", "Decrease Brightness by 8", "Menu")]
+    public void SectorHeightCommandsMatchUdbActionSurface(string id, string title, string defaultGesture)
     {
         var command = EditorCommandCatalog.Find(id);
 
         Assert.NotNull(command);
         Assert.Equal(title, command.Title);
-        Assert.Equal("Menu", command.DefaultGesture);
+        Assert.Equal(defaultGesture, command.DefaultGesture);
         Assert.Equal(EditorCommandScope.Map2D, command.Scope);
         Assert.True(command.AllowKeys);
         Assert.True(command.AllowMouse);
         Assert.True(command.AllowScroll);
         Assert.True(command.Repeat);
+    }
+
+    [Fact]
+    public void UdbSectorHeightCommandsOwnDefaultShortcutResolution()
+    {
+        Assert.Equal("map2d.raisefloor8", EditorCommandCatalog.ResolveShortcut(
+            EditorCommandScope.Map2D, EditorPointerInput.ScrollUp, accelerator: true, alt: true));
+        Assert.Equal("map2d.lowerfloor8", EditorCommandCatalog.ResolveShortcut(
+            EditorCommandScope.Map2D, EditorPointerInput.ScrollDown, accelerator: true, alt: true));
+        Assert.Equal("map2d.raiseceiling8", EditorCommandCatalog.ResolveShortcut(
+            EditorCommandScope.Map2D, EditorPointerInput.ScrollUp, shift: true, alt: true));
+        Assert.Equal("map2d.lowerceiling8", EditorCommandCatalog.ResolveShortcut(
+            EditorCommandScope.Map2D, EditorPointerInput.ScrollDown, shift: true, alt: true));
     }
 
     [Fact]
